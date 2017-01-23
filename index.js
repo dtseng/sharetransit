3f44e47c5e47ceab691492803a5cd15c09e70937
David Tseng <dtseng96@gmail.com>
2017-01-22 18:44:13 -0800

    fixing non-update problem

--- index.js
+++ index.js
@@ -100,30 +100,30 @@ function sendGenericMessage(recipientId, messageText) {
 function sendTextMessage(recipientId, messageText) {
 	//agency, bus number, stop number
 	console.log("messagetext: " + messageText)
-	splitText = messageText.split(" ")
-	agency = splitText[0]
-	busNumber = splitText[1]
-	stopNumber = splitText[2]
-	requestString = api + 'agencies/' + agency + '/routes/' + busNumber + '/stops/' + stopNumber + '/predictions'
-	console.log('requestString ' + requestString)
+	// splitText = messageText.split(" ")
+	// agency = splitText[0]
+	// busNumber = splitText[1]
+	// stopNumber = splitText[2]
+	// requestString = api + 'agencies/' + agency + '/routes/' + busNumber + '/stops/' + stopNumber + '/predictions'
+	// console.log('requestString ' + requestString)
 
-		request(requestString, function(error, response, body) {
-			if (error)
-				sendMessage = "error" + error
-			else
-				sendMessage = JSON.parse(body)[0]['values'][0]['minutes'] + ' minutes'
-			console.log(sendMessage)
+	// 	request(requestString, function(error, response, body) {
+	// 		if (error)
+	// 			sendMessage = "error" + error
+	// 		else
+	// 			sendMessage = JSON.parse(body)[0]['values'][0]['minutes'] + ' minutes'
+	// 		console.log(sendMessage)
 
-			var messageData = {
-		    recipient: {
-		      id: recipientId
-		    },
-		    message: {
-		      text: sendMessage
-		    }
-		  };
-		  callSendAPI(messageData);
-		})
+	// 		var messageData = {
+	// 	    recipient: {
+	// 	      id: recipientId
+	// 	    },
+	// 	    message: {
+	// 	      text: sendMessage
+	// 	    }
+	// 	  };
+	// 	  callSendAPI(messageData);
+	// 	})
 }
 
 function callSendAPI(messageData) {
